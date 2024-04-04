3c966e7bb06d80f66c77e41c291febc1b456a9e2
AttoCat <contact@attocat.com>
2024-04-04 09:56:17 +0900

    :white_check_mark: Test login

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { login } from "../src/Login.tsx";
 import { auth } from "./auth/index.ts";
 import { hook } from "./hook/index.ts";
 
@@ -7,6 +8,7 @@ const app = new Hono();
 app.get("*", (c) => c.text("Hello!"));
 app.route("/api/hook", hook);
 app.route("/api/auth", auth);
+app.route("/login", login);
 
 export default app;
 
