3e77db934a2ea1e1671d0c8b9e02663bc3faf542
AttoCat <contact@attocat.com>
2024-04-21 21:31:04 +0900

    :speech_balloon: Edit text

--- api/hook/user.ts
+++ api/hook/user.ts
@@ -13,20 +13,19 @@ export const getUserInfo = async (
 };
 
 export const composeUserInfo = (user: Tables<"profiles">): TextMessage => {
-  const text = `
-  あなたのエントリー情報は以下の通りです。
+  const text = `あなたのエントリー情報は以下の通りです。
 
-  エントリーナンバー: ${user.entry_number}
-  名前: ${user.full_name}
-  ふりがな: ${user.full_name_kana}
-  ハンドルネーム: ${user.handle_name}
-  所属教育機関: ${user.school}
-  本名を使用しない場所: ${user.privacy}
+エントリーナンバー: ${user.entry_number}
+名前: ${user.full_name}
+ふりがな: ${user.full_name_kana}
+ハンドルネーム: ${user.handle_name}
+所属教育機関: ${user.school}
+本名を使用しない場所: ${user.privacy}
 
-  変更を希望する場合、公式Twitter(X)または sichihenge.cup@gmail.com までご連絡ください。
-  エントリーのキャンセルは以下のフォームからお願いします:
-  https://forms.gle/SrDiCZchXhxhRGdV9
-  `;
+変更を希望する場合、公式Twitter(X)または sichihenge.cup@gmail.com までご連絡ください。
+
+エントリーのキャンセルは以下のフォームからお願いします:
+https://forms.gle/SrDiCZchXhxhRGdV9`;
   const message: TextMessage = { "type": "text", text: text };
   return message;
 };
