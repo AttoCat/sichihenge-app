1171f8e5fa0f1970e8646883299d9364693c7c74
AttoCat <contact@attocat.com>
2024-05-15 10:31:39 +0900

    Update index.tsx

--- api/auth/index.tsx
+++ api/auth/index.tsx
@@ -141,9 +141,9 @@ auth.post("/signup", async (c) => {
 const getNextEntryNumber = async () => {
   const { data: lastUser, error ,count} = await supabase
     .from("profiles").select("*",{count:"exact",head:true})
-  if (!lastUser) {
-    return null;
+  if (!count) {
+    return 0;
   }
   console.log("Current last user id" ,count)
-  return count! + 1;
+  return count + 1;
 };
