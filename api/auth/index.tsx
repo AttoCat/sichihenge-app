7854743ef5ffa233e55d32396bdd85dc7d653fc9
AttoCat <contact@attocat.com>
2024-05-17 22:05:31 +0900

    :recycle: Format

--- api/auth/index.tsx
+++ api/auth/index.tsx
@@ -139,11 +139,16 @@ auth.post("/signup", async (c) => {
 });
 
 const getNextEntryNumber = async () => {
-  const { data: lastUser, error ,count} = await supabase
-    .from("profiles").select("*",{count:"exact",head:true})
+  const {
+    data: lastUser,
+    error,
+    count,
+  } = await supabase
+    .from("profiles")
+    .select("*", { count: "exact", head: true });
   if (!lastUser) {
     return null;
   }
-  console.log("Current last user id" ,count)
+  console.log("Current last user id", count);
   return count! + 1;
 };
